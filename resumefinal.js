146329cea2ddc7a9d4979f6d336e0c15a0b564bc
sesbrenosienna <ana_sesbreno@dlsu.edu.ph>
2020-08-11 14:47:37 +0800

    finalversion

---
+++ resumefinal.js
@@ -0,0 +1,89 @@
+// to change font color to gray and to change background color to pink of first
+function afunction() {
+    document.getElementById("category1").style.color = "gray";  
+    document.getElementById("about").style.color = "palevioletred"; 
+
+    document.getElementById("category2").style.color = "black";
+    document.getElementById("educ").style.color = "black";
+    
+
+    document.getElementById("category3").style.color = "black";
+    document.getElementById("org").style.color = "black";
+    
+    document.getElementById("category4").style.color = "black";
+    document.getElementById("work").style.color = "black";
+    
+    document.getElementById("category5").style.color = "black";
+    document.getElementById("project").style.color = "black";
+}
+
+function efunction() {
+    document.getElementById("category2").style.color = "gray";  
+    document.getElementById("educ").style.color = "palevioletred"; 
+
+    document.getElementById("category1").style.color = "black";
+    document.getElementById("about").style.color = "black";
+    
+
+    document.getElementById("category3").style.color = "black";
+    document.getElementById("org").style.color = "black";
+    
+    document.getElementById("category4").style.color = "black";
+    document.getElementById("work").style.color = "black";
+    
+    document.getElementById("category5").style.color = "black";
+    document.getElementById("project").style.color = "black";
+}
+
+function ofunction() {
+    document.getElementById("category3").style.color = "gray";  
+    document.getElementById("org").style.color = "palevioletred"; 
+
+    document.getElementById("category1").style.color = "black";
+    document.getElementById("about").style.color = "black";
+    
+    document.getElementById("category2").style.color = "black";
+    document.getElementById("educ").style.color = "black";
+    
+    document.getElementById("category4").style.color = "black";
+    document.getElementById("work").style.color = "black";
+    
+    document.getElementById("category5").style.color = "black";
+    document.getElementById("project").style.color = "black";
+}
+
+function wfunction() {
+    document.getElementById("category4").style.color = "gray";  
+    document.getElementById("work").style.color = "palevioletred"; 
+
+    document.getElementById("category1").style.color = "black";
+    document.getElementById("about").style.color = "black";
+    
+    document.getElementById("category2").style.color = "black";
+    document.getElementById("educ").style.color = "black";
+    
+    document.getElementById("category3").style.color = "black";
+    document.getElementById("org").style.color = "black";
+    
+    document.getElementById("category5").style.color = "black";
+    document.getElementById("project").style.color = "black";
+}
+
+function pfunction() {
+    document.getElementById("category5").style.color = "gray";  
+    document.getElementById("project").style.color = "palevioletred"; 
+
+    document.getElementById("category1").style.color = "black";
+    document.getElementById("about").style.color = "black";
+    
+    document.getElementById("category2").style.color = "black";
+    document.getElementById("educ").style.color = "black";
+    
+    document.getElementById("category3").style.color = "black";
+    document.getElementById("org").style.color = "black";
+    
+    document.getElementById("category4").style.color = "black";
+    document.getElementById("work").style.color = "black";
+}
+
+
